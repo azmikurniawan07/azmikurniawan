db582c2d860004b869cf7c971e67a4701a2bb86a
azmikurniawan07 <azmikurniawan350@gmail.com>
2023-10-31 10:48:34 +0700

    Portofolio

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+
+
+import { useState } from "react";
+import { Navbar, Hero, SideMenu, About } from "./components";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Resume from "./components/Resume";
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <div className="relative z-0 bg-[#181818]">
+                  <Hero />
+                  <About />
+                  <SideMenu />
+                </div>
+              </>
+            }
+          />
+          <Route path="/resume" element={<Resume/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
