d196c9bc0d626f84ae94a0d663aa79b8a384a4c5
azmikurniawan07 <azmikurniawan350@gmail.com>
2023-10-31 11:28:49 +0700

    Portofolio

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
+import react from '@vitejs/plugin-react'
+import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),svgr()],
+  base: '/azmikurniawan/', // Sesuaikan dengan URL repository GitHub Pages Anda
+  build: {
+    outDir: 'dist', // Direktori output untuk build produksi
+  },
 })
